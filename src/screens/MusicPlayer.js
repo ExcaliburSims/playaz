90c189deeb56b770f9e4df9e4af64a5d2ba6b412
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-23 12:35:59 +0100

    feat: add style

--- src/screens/MusicPlayer.js
+++ src/screens/MusicPlayer.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const MusicPlayer = () => {
 	return (
-		<View>
-			<Text>MusicPlayer</Text>
+		<View style={styles.container}>
+			<Text>Music Player</Text>
 		</View>
 	)
 }
 
 export default MusicPlayer
+
+
+const styles = StyleSheet.create({
+  container: {
+		flex: 1,
+		backgroundColor: '#555',
+		alignItems: 'center',
+		justifyContent: 'center',
+  }
+})
