5788ede9aacfa9b9d436381541d458a1786df190
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-23 13:20:21 +0100

    feat: add a add-alert icon

--- src/screens/MusicPlayer.js
+++ src/screens/MusicPlayer.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const MusicPlayer = () => {
 	return (
 		<View style={styles.container}>
+			<Icon name="add-alert" size={30} color="#900" />
 			<Text>Music Player</Text>
 		</View>
 	)
