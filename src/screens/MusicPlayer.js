8f6926f366a40050a434f0c958e2e9285da9c37e
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-23 11:58:31 +0100

    feat: create src

---
+++ src/screens/MusicPlayer.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MusicPlayer = () => {
+	return (
+		<View>
+			<Text>MusicPlayer</Text>
+		</View>
+	)
+}
+
+export default MusicPlayer
