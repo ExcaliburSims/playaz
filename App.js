f15e24ac5b46b188961fe6c7b116bec1512a3ed9
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-23 12:35:45 +0100

    feat: add style

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import MusicPlayer from './src/screens/MusicPlayer'
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <MusicPlayer />
     </View>
   )
 }
 
 export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
