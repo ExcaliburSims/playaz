81e20f0bd0de1984426b485f79b28fc5f922cefa
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-24 22:29:25 +0100

    feat: create a first slider

---
+++ src/components/Slider.js
@@ -0,0 +1,36 @@
+import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
+import React from 'react'
+
+const SliderApp = () => {
+	return (
+		<SafeAreaView>
+			<View>
+				<Text>Playlist</Text>
+				<Image
+					style={styles.picture}
+					source={require('../assets/images/img1.jpg')}
+				/>
+			</View>
+		</SafeAreaView>
+	)
+}
+
+export default SliderApp
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#1F172F',
+	},
+	maincontent: {
+		marginVertical: 15,
+		//backgroundColor: 'red',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	picture: {
+		width: 200,
+		height: 200,
+		borderRadius: 31,
+	},
+})
