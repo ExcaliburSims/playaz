7bff9bade5f3f1dcfbb83b23fe2da9de71fd1fd3
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-24 15:51:43 +0100

    feat: add background

--- src/screens/MusicPlayer.js
+++ src/screens/MusicPlayer.js
@@ -1,13 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const MusicPlayer = () => {
 	return (
-		<View style={styles.container}>
-			<Icon name="add-alert" size={30} color="#900" />
-			<Text>Music Player</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View>
+				<Icon name="add-alert" size={30} color="#900" />
+				<Text>Music Player</Text>
+			</View>
+		</SafeAreaView>
 	)
 }
 
@@ -17,7 +19,7 @@ export default MusicPlayer
 const styles = StyleSheet.create({
   container: {
 		flex: 1,
-		backgroundColor: '#555',
+		backgroundColor: '#1F172F',
 		alignItems: 'center',
 		justifyContent: 'center',
   }
