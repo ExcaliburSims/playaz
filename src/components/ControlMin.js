db1867d9efabff8c4c854c23b8fe2fcd9cc0b17e
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-25 15:07:57 +0100

    feat: style control panel

--- src/components/ControlMin.js
+++ src/components/ControlMin.js
@@ -1,11 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native'
 import React from 'react'
+import Icone from 'react-native-vector-icons/MaterialIcons';
 
 const ControlMin = () => {
 	return (
-		<View style={styles.maincontent}>
-			<Text>controlMin</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.maincontent}>
+				<Icone
+						name="close"
+						size={30}
+						color="white"
+				/>
+				<Icone
+						name="fast-rewind"
+						size={30}
+						color="white"
+				/>
+				<Image
+						style={styles.picture}
+						source={require('../assets/images/img1.jpg')}
+					/>
+				<Icone
+						name="pause"
+						size={30}
+						color="white"
+				/>
+				<Icone
+						name="fast-forward"
+						size={30}
+						color="white"
+					/>
+			</View>
+		</SafeAreaView>
 	)
 }
 
@@ -13,18 +39,21 @@ export default ControlMin
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#1F172F',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		backgroundColor: 'green',
+		alignItems: 'center',
 	},
 	maincontent: {
-		marginVertical: 50,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
 		backgroundColor: 'red',
 		alignItems: 'center',
-		justifyContent: 'center',
+		width: 300,
 	},
 	picture: {
-		width: 200,
-		height: 200,
-		borderRadius: 31,
+		width: 50,
+		height: 50,
+		borderRadius: 25,
 	},
 })
