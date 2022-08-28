7636aab26b6b6c18e514dc357b437c36fb8d5237
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-28 18:50:45 +0100

    feat: add music image

--- src/screens/Player.js
+++ src/screens/Player.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions, Image } from 'react-native'
 import React from 'react'
 import Icone from 'react-native-vector-icons/Ionicons';
 
@@ -9,13 +9,24 @@ const Player = () => {
 		<SafeAreaView style={styles.container}>
 			<View style={styles.maincontainer}>
 				{/* image */}
+				<View style={[styles.imagewrap, styles.elevation]}>
+					<Image
+						source={require('../assets/images/img2.jpg')}
+						style={styles.musicimage}
+					/>
+				</View>
+				{/* son title */}
+				<View>
+					<Text></Text>
+					<Text></Text>
+				</View>
 
 				{/* slider */}
 
 				{/* panel controler */}
 			</View>
 			<View style={styles.bottomcontainer}>
-				<View style={styles.bottomcontainerWrap}>
+				<View style={styles.bottomcontainerwrap}>
 				<TouchableOpacity>
 				<Icone
 						name="heart-outline"
@@ -71,9 +82,31 @@ const styles = StyleSheet.create({
 		borderWidth: 1,
 		borderTopColor: '#393E46'
 	},
-	bottomcontainerWrap: {
+	bottomcontainerwrap: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		width: '80%',
+	},
+	imagewrap: {
+		width: 300,
+		height: 340,
+		marginBottom: 25,
+		borderRadius: 25,
+		//backgroundColor: 'green',
+	},
+	musicimage: {
+		width: '100%',
+		height: '100%',
+		borderRadius: 25,
+	},
+	elevation: {
+		elevation: 5,
+		shadowColor: '#ccc',
+		shadowOffset: {
+			width: 5,
+			height: 5,
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 3.84,
 	},
 })
