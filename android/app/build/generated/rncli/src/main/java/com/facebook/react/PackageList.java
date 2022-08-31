f754dbe1bec69cb28a62605f3dfb4af499ddb6c1
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-31 11:18:51 +0100

    feat: add react-native track music player

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 
 // @react-native-community/slider
 import com.reactnativecommunity.slider.ReactSliderPackage;
+// react-native-track-player
+import com.doublesymmetry.trackplayer.TrackPlayer;
 // react-native-vector-icons
 import com.oblador.vectoricons.VectorIconsPackage;
 
@@ -61,6 +63,7 @@ public class PackageList {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
       new ReactSliderPackage(),
+      new TrackPlayer(),
       new VectorIconsPackage()
     ));
   }
