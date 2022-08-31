a39ed1933939700686d4d50f9b57f5d099cc6981
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-31 15:22:40 +0100

    feat: config react native track player

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
+
+TrackPlayer.registerPlaybackService(() => require('./service'));
