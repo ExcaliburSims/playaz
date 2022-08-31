78c2ed067d2826ae63f7afac55b2a08cf0cc060c
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-31 15:59:43 +0100

    feat: config react native track player

--- service.js
+++ service.js
@@ -1,28 +1,11 @@
-import TrackPlayer from "react-native-track-player";
+import TrackPlayer, { Event } from "react-native-track-player";
 
 module.exports = async function() {
     // This service needs to be registered for the module to work
     // but it will be used later in the "Receiving Events" section
 	try {
-    TrackPlayer.addEventListener('remote-play', () => {
-      TrackPlayer.play()
-    })
-
-    TrackPlayer.addEventListener('remote-pause', () => {
-      TrackPlayer.pause()
-    });
-
-    TrackPlayer.addEventListener('remote-next', () => {
-      TrackPlayer.skipToNext()
-    });
-
-    TrackPlayer.addEventListener('remote-previous', () => {
-      TrackPlayer.skipToPrevious()
-    });
-
-    TrackPlayer.addEventListener('remote-stop', () => {
-      TrackPlayer.destroy()
-    });
+		TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
 	} catch (error) {
 		console.log(error);
 	}
